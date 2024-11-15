01cf59c536e312e69442fca4d00611665c54e8fe
armando.jumawid <114337577+Arman0511@users.noreply.github.com>
2024-11-15 08:51:02 +0800

    Create main.c
    name

---
+++ Code/BERA Requestor App/windows/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <sdlib.h>
+
+// Fuction to add two numbers
+int add(int a, int b) {
+    return a + b;
+}
+
+// Fuction to substract two numbers
+int substact(int a, int b) {
+    return a - b;    
+}
+
+// Fuction to multiply two numbers
+int multiply(int a, int b) {
+    return a * b;    
+}
+
+// Function to Divide two numbers
+int divide(int a, int b) 
+    if (b == 0) {
+       printf("Error: Division by zero!\n");
+       exit(1);
+    }
+    return 0;
+}
+
+int main () {
+
+
+
+
+
+
+
+  return 0;
+}
